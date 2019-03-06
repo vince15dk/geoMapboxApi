3d09f58068ec28cb94013b9fb91bef30bdbeb92a
SJ <vince15dk@gmail.com>
2019-03-06 21:12:48 +0900

    push to heroku

--- src/app.js
+++ src/app.js
@@ -18,7 +18,6 @@ const partialsPath = path.join(__dirname, '../templates/partials')
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
-
 // Setup static directory to serve
 app.use(express.static(publicdirectoryPath))
 
